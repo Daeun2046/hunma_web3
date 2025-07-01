bc6f8e7afc459401d157153eeeb0dc0c2d75a1e6
DaEun <128888140+Daeun2046@users.noreply.github.com>
2025-07-02 01:04:13 +0900

    Update ERC20.sol
    - anvil을 이용해서 local testnet을 만든다.
    - Local testnet에 forge를 이용해서 해당 contract를 배포하고 특정 주소에 본인이 만든 Token을 어느 정도 충전해준다.
    - cast를 이용해서 그 특정 주소에 대해서 내가 만든 token의 balance를 조회한다.

--- ERC20.sol
+++ ERC20.sol
@@ -8,12 +8,29 @@ contract ERC20 {
     uint8 public decimals = 18;
     uint256 public totalSupply;
 
+<<<<<<< Updated upstream
+=======
+    address public owner;
+
+>>>>>>> Stashed changes
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
 
+<<<<<<< Updated upstream
+=======
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+>>>>>>> Stashed changes
     function transfer(address _to, uint256 _value) public returns (bool) {
         require(balanceOf[msg.sender] >= _value, "Not enough balance");
         balanceOf[msg.sender] -= _value;
@@ -38,16 +55,28 @@ contract ERC20 {
         return true;
     }
 
+<<<<<<< Updated upstream
     function mint(address to, uint256 amount) public {
+=======
+    function mint(address to, uint256 amount) public onlyOwner {
+>>>>>>> Stashed changes
         totalSupply += amount;
         balanceOf[to] += amount;
         emit Transfer(address(0), to, amount);
     }
 
+<<<<<<< Updated upstream
     function burn(address from, uint256 amount) public {
             require(balanceOf[from] >= amount, "Insufficient balance");
             totalSupply -= amount;
             balanceOf[from] -= amount;
             emit Transfer(from, address(0), amount);
+=======
+    function burn(address from, uint256 amount) public onlyOwner {
+        require(balanceOf[from] >= amount, "Insufficient balance");
+        totalSupply -= amount;
+        balanceOf[from] -= amount;
+        emit Transfer(from, address(0), amount);
+>>>>>>> Stashed changes
     }
 }
